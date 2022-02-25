663680d78cdec9d834078c580baaa38ed072cb99
Ronald Beltrán <71865035+rbeltran076@users.noreply.github.com>
2022-02-25 12:26:03 -0500

    IVT con  4 sensores y 4 vibradores.
    Añadí los motores y los sensores frontales.

--- vibration_motors_version.ino
+++ vibration_motors_version.ino
@@ -1,12 +1,19 @@
 // VARIABLES
-int trig1 = 9;
-int echo1 = 8;
-int trig2 = 7;
-int echo2 = 6;
+int trig1 = 2;
+int echo1 = 3;
+int trig2 = 4;
+int echo2 = 5;
+int trig3 = 6;
+int echo3 = 7;
+int trig4 = 8;
+int echo4 = 9;
+
 int motorPin1 = 10;
 int motorPin2 = 11;
+int motorPin3 = 12;
+int motorPin4 = 13;
 int counter = 0;
-int dist1, dist2, inten1, inten2;
+int dist1, inten1, dist2, inten2, dist3, inten3, dist4, inten4;
 
 // SETUP
 void setup()
@@ -18,14 +25,22 @@ void setup()
   pinMode (echo1, INPUT);
   pinMode (trig2, OUTPUT);
   pinMode (echo2, INPUT);
-
+  pinMode (trig3, OUTPUT);
+  pinMode (echo3, INPUT);
+  pinMode (trig4, OUTPUT);
+  pinMode (echo4, INPUT);
+  
 //Activar los sensores de proximidad.
   digitalWrite (trig1, LOW);
   digitalWrite (trig2, LOW);
+  digitalWrite (trig3, LOW);
+  digitalWrite (trig4, LOW);
 
 // Definir los O e I de los motores
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
 
 // Vibracion de bienvenida
   analogWrite(motorPin1, 75);
@@ -59,27 +74,43 @@ void loop()
   // Medidas de distancias de los sensores 1 y 2
   dist1 = measure(trig1, echo1);
   dist2 = measure(trig2, echo2);
-  Serial.print("Distancia1 ");
+  dist3 = measure(trig3, echo3);
+  dist4 = measure(trig4, echo4);
+  Serial.print("Distancia 1 ");
   Serial.println (dist1);
-  Serial.print("Distancia2 ");
+  Serial.print("Distancia 2 ");
   Serial.println (dist2);
+  Serial.print("Distancia 3 ");
+  Serial.println (dist3);
+  Serial.print("Distancia 4 ");
+  Serial.println (dist4);
 
   // Intensidad es inv. prop. a la distancia
   inten1 = 100 - dist1;
   inten2 = 100 - dist2;
+  inten3 = 100 - dist3;
+  inten4 = 100 - dist4;
 
   // Imprimir los datos en el monitor serial.
   Serial.println(inten1);
   Serial.println(inten2);
+  Serial.println(inten3);
+  Serial.println(inten4);
   Serial.println("___________________________________________");
 
 
  // Si la distancia es menor o igual a 150 mm en un sensor, vibrar con la intensidad en ese sensor.
   if (dist1 <= 100){
-    // Vibrar
   analogWrite(motorPin1, inten1);
   }
   if (dist2 <= 100){
   analogWrite(motorPin2, inten2);
   }
+  if (dist3 <= 100){
+  analogWrite(motorPin3, inten3);
+  }
+  if (dist4 <= 100){
+  analogWrite(motorPin4, inten4);
+  }
 }
+
